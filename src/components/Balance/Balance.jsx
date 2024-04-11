a2fed9dae0afc6c870f3b1033efff242a0cfbf4f
RaresDrg <dragan_rares13@yahoo.com>
2024-04-12 01:18:26 +0300

    copy project
    copy project

---
+++ src/components/Balance/Balance.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+
+import { selectBalance } from '../../redux/auth/selectors';
+import styles from './Balance.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInfo } from '../../redux/auth/operations';
+
+function Balance() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUserInfo());
+  }, [dispatch]);
+
+  const balance = useSelector(selectBalance);
+
+  return (
+    <div className={styles.balance}>
+      <h3>Your balance</h3>
+      <p>
+        ₴ <span>{balance ? balance.toFixed(2) : '0.00'}</span>
+      </p>
+    </div>
+  );
+}
+
+export default Balance;
